351ac318be737b649c6ec6251dd8d18371d63ca2
Agata <buchwaldagata@gmail.com>
2024-02-27 21:56:51 +0100

    Add tsp reader

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        TSPReader("src/resources/kroA100.tsp");
+        TSPReader("src/resources/kroB100.tsp");
     }
+
+    private static void TSPReader(String path) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }

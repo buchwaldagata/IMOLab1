73379cff07f5cb3c22164c3e99664c3c90711c17
buchwaldagata <buchwaldagata@gmail.com>
2024-03-08 22:17:54 +0100

    Refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,28 +8,27 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        countEuclideanDistanceFromFile("src/resources/kroA100.tsp");
-        countEuclideanDistanceFromFile("src/resources/kroB100.tsp");
+        String distanceMatrixKroA = countEuclideanDistanceFromFile("src/resources/kroA100.tsp");
+        String distanceMatrixKroB = countEuclideanDistanceFromFile("src/resources/kroB100.tsp");
+        System.out.println(distanceMatrixKroA);
     }
 
     private static String countEuclideanDistanceFromFile(String path){
         List<List<String>> preparedLinesFromFile = prepareLinesFromFile(path);
-        String euclideanDistance = getEuclideanDistance(preparedLinesFromFile);
-        return euclideanDistance;
+        return getEuclideanDistance(preparedLinesFromFile);
     }
 
 
     private static List<List<String>> prepareLinesFromFile(String path) {
         List<List<String>> correctsLines = new ArrayList<>();
-        Integer count = -1;
+        int count = -1;
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
                 List<String> correctLine = new ArrayList<>();
                 correctLine.add(line);
                 count++;
-                if (count > 5 && count < 106) {
+                if (count > 5 && count < 106) { //todo: change 5 and 106
                     correctsLines.add(correctLine);
                 }
             }
@@ -40,37 +39,32 @@ public class Main {
     }
 
     private static String getEuclideanDistance(List<List<String>> preparedLinesFromFile){
+        StringBuilder distanceMatrix = new StringBuilder();
         if (preparedLinesFromFile.size() > 1) {
             for (int i = 0; i < preparedLinesFromFile.size(); i++) {
                 for (int j = 0; j < preparedLinesFromFile.size(); j++){
                     String[] firstList = preparedLinesFromFile.get(i).get(0).split("\\s+");
-                    String numberFirst = firstList[0];
-                    int first = Integer.parseInt(numberFirst);
                     int x1 = Integer.parseInt(firstList[1]);
                     int y1 = Integer.parseInt(firstList[2]);
 
-
                     String[] secondList = preparedLinesFromFile.get(j).get(0).split("\\s+");
-                    String numberSecond = secondList[0];
-                    int dwa = Integer.parseInt(numberSecond);
                     int x2 = Integer.parseInt(secondList[1]);
                     int y2 = Integer.parseInt(secondList[2]);
 
                     Long countEuclideanDistance = countEuclideanDistance(x1, y1, x2, y2);
-                    System.out.print(countEuclideanDistance + " ");
+                    distanceMatrix.append(countEuclideanDistance).append(" ");
                 }
-                System.out.println();
+                distanceMatrix.append("\n");
             }
         }
-        return "hej";
+        return distanceMatrix.toString();
     }
 
     private static Long countEuclideanDistance(Integer x1, Integer y1, Integer x2, Integer y2){
-        Integer squareFirstSubstraction = (x2-x1)*(x2-x1);
-        Integer squareSecondSubstraction = (y2-y1)*(y2-y1);
-        Double distance =Math.sqrt(squareFirstSubstraction+squareSecondSubstraction);
-        Long roundDistance = Math.round(distance);
-        return roundDistance;
+        Integer squareFirstSubtraction = (x2-x1)*(x2-x1);
+        Integer squareSecondSubtraction = (y2-y1)*(y2-y1);
+        double distance = Math.sqrt(squareFirstSubtraction+squareSecondSubtraction);
+        return Math.round(distance);
     }
 
 
